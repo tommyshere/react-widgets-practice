7e4c4a1daab8c2cdc00a845548ed736657b90ab8
Tommy <tommyshere@gmail.com>
2021-04-09 13:30:23 -0700

    accordion styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Counter from './components/Counter';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components'
+  }
+]
 
 const App = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
+      <Counter />
     </div>
   )
 }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClicked = (index) => {
+    setActiveIndex(index);
+  };
+
+
+
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
+    return (
+      <React.Fragment key={item.title}>
+        <div 
+          className={`title ${active}`}
+          onClick={() => onTitleClicked(index)}
+          >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    )
+  });
+
   return (
-    <h1>Accordion</h1>
+    <div className="ui styled accordion">
+      {renderedItems}
+    </div>
   )
 };
 
